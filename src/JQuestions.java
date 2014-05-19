b4d5a9b0a63dc82e89605597935a15648ab33ddc
Tarjei400 <adrian.jutrowski@gmail.com>
2014-05-19 23:32:25 +0200

    Added shuffling question from loaded csv (untested yet)
    Signed-off-by: Tarjei400 <adrian.jutrowski@gmail.com>

--- src/JQuestions.java
+++ src/JQuestions.java
@@ -4,10 +4,11 @@ import au.com.bytecode.opencsv.*;
 import java.util.Arrays;
 
 public class JQuestions {
-	public static String m_fileName = "../jepardy-flat-file.csv";
+	public static String m_fileName = "../jeperdy-flat-file.csv";
 	public List<String> m_questions;
 	
 	public JQuestions() {
+		m_questions = new ArrayList<String>();
 		CSV csv = CSV
 			    .separator(',')  // delimiter of fields
 			    .quote('"')      // quote character
@@ -15,8 +16,24 @@ public class JQuestions {
 		csv.read(m_fileName, new CSVReadProc() {
 		    public void procRow(int rowIndex, String... values) {
 		        System.out.println(rowIndex + ": " + Arrays.asList(values));
+
 		    }
 		});
 		
+	}
+	public List<String> shuffle(int amount) throws Exception{
+		if (m_questions.size() < amount)
+			throw new Exception("You dont have enought question to shuffle that many of them");
+		
+		List<String> ret = new ArrayList<String>();
+		Random r = new Random();
+		
+		for (int i = 0; i < amount; i++){
+			int random = r.nextInt() % m_questions.size();
+			ret.add(m_questions.get(random));
+		}
+		
+		
+		return ret;
 	}
 }

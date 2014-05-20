886707893f85c0f07221573711bfe2f14e197f91
Tarjei400 <adrian.jutrowski@gmail.com>
2014-05-20 14:49:47 +0200

    Added SearchEntry
    Signed-off-by: Tarjei400 <adrian.jutrowski@gmail.com>

---
+++ src/SearchEntry.java
@@ -0,0 +1,14 @@
+
+public class SearchEntry {
+	double m_score;
+	String m_title;
+	
+	public SearchEntry(){
+	}
+	
+	double getScore(){ return m_score;}
+	String getTitle(){ return m_title;}
+	void setScore(double score){ m_score = score;}
+	void setTitle(String title){ m_title = title;}
+
+}
